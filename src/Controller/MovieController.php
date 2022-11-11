fb8b9e663e0fb8de2baa17a1e88b4b390f3fb0d3
Mouad Anouch <md.anouchpro@gmail.com>
2022-11-11 23:16:05 +0100

    remove useless code

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -23,38 +23,12 @@ class MovieController {
         $this->requestRoute = $requestRoute;
         $this->params = $params;
         $this->apikey = "046720553f711abd5a48ddf1b60db681";
-        // $this->movieGateway = new MovieGateway($db);
-        // $this->imbGateaway = new ImbdGateway($db);
     }
 
 
     // The database routes
     private function moviesRoute()
     {
-        // switch ($this->requestMethod) {
-        //     case 'GET':
-        //         if ($this->userId) {
-        //             $response = $this->getUser($this->userId);
-        //         } else {
-        //             $response = $this->getAllUsers();
-        //         };
-        //         break;
-        //     case 'POST':
-        //         $response = $this->createUserFromRequest();
-        //         break;
-        //     case 'PUT':
-        //         $response = $this->updateUserFromRequest($this->userId);
-        //         break;
-        //     case 'DELETE':
-        //         $response = $this->deleteUser($this->userId);
-        //         break;
-        //     default:
-        //         $response = $this->notFoundResponse();
-        //         break;
-        // }
-        //  if ($response) {
-        //     echo $response;
-        // }
         echo 'empty';
     }
     
@@ -99,7 +73,6 @@ class MovieController {
 
     public function processRequest()
     {
-
         if ($this->requestRoute == 'movie') {
            $this->moviesRoute();
         }
@@ -109,95 +82,8 @@ class MovieController {
         else{
             echo "no route found";
         }
-       
-       
     }
 
-    private function getAllUsers()
-    {
-        $result = $this->movieGateway->findAll();
-        $response['status_code_header'] = 'HTTP/1.1 200 OK';
-        $response['body'] = json_encode($result);
-        return $response;
-    }
-
-    private function getUser($id)
-    {
-        $result = $this->movieGateway->find($id);
-        if (! $result) {
-            return $this->notFoundResponse();
-        }
-        $response['status_code_header'] = 'HTTP/1.1 200 OK';
-        $response['body'] = json_encode($result);
-        return $response;
-    }
-
-    private function createUserFromRequest()
-    {
-        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
-        if (! $this->validateMovie($input)) {
-            return $this->unprocessableEntityResponse();
-        }
-        $this->movieGateway->insert($input);
-        $response['status_code_header'] = 'HTTP/1.1 201 Created';
-        $response['body'] = null;
-        return $response;
-    }
-
-    private function updateUserFromRequest($id)
-    {
-        $result = $this->movieGateway->find($id);
-        if (! $result) {
-            return $this->notFoundResponse();
-        }
-        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
-        if (! $this->validateMovie($input)) {
-            return $this->unprocessableEntityResponse();
-        }
-        $this->movieGateway->update($id, $input);
-        $response['status_code_header'] = 'HTTP/1.1 200 OK';
-        $response['body'] = null;
-        return $response;
-    }
-
-    private function deleteUser($id)
-    {
-        $result = $this->movieGateway->find($id);
-        if (! $result) {
-            return $this->notFoundResponse();
-        }
-        $this->movieGateway->delete($id);
-        $response['status_code_header'] = 'HTTP/1.1 200 OK';
-        $response['body'] = null;
-        return $response;
-    }
-
-    private function validateMovie($input)
-    {
-        if (! isset($input['firstname'])) {
-            return false;
-        }
-        if (! isset($input['lastname'])) {
-            return false;
-        }
-        return true;
-    }
-
-    private function unprocessableEntityResponse()
-    {
-        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
-        $response['body'] = json_encode([
-            'error' => 'Invalid input'
-        ]);
-        return $response;
-    }
-
-    private function notFoundResponse()
-    {
-        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
-        $response['body'] = null;
-        return $response;
-    }
 
     function callAPI($method, $url, $data){
         $curl = curl_init();
